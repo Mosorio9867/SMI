972aab76132d961b8f1b903ec0d0bfaacae441db
MANUEL OSORIO <manuel.osorio@intelligenia.com>
2020-04-10 16:15:36 -0500

    add doctors

--- src/app/main/pages/doctors/doctors.component.ts
+++ src/app/main/pages/doctors/doctors.component.ts
@@ -1,5 +1,8 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {fuseAnimations} from '@fuse/animations';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
     selector: 'app-doctors',
@@ -10,9 +13,25 @@ import {fuseAnimations} from '@fuse/animations';
 })
 export class DoctorsComponent implements OnInit {
 
-    constructor() {
+    public form: FormGroup;
+    public displayedColumns: string[] = ['name', 'identification', 'age', 'actions'];
+    public dataSource = new MatTableDataSource<any>(ELEMENT_DATA);
+
+    @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+    constructor(
+        private _formBuilder: FormBuilder
+    ) {
+        this.form = this._formBuilder.group({
+            search: [null]
+        })
     }
 
     ngOnInit(): void {
+        this.dataSource.paginator = this.paginator;
     }
 }
+
+const ELEMENT_DATA: any[] = [
+    {name: 'Manuel Enrique Osorio Ochoa', identification: '1047506765', age: 21, active: true},
+];
